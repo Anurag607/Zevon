cbd96f1be284c9ae7bcc45cad94eb9ff3bf9f4aa
Anurag <anurag79002@gmail.com>
2023-01-27 20:06:51 +0530

    updated .gitignore

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5fb1d3e2b307e0b5"; }
+/******/ 		__webpack_require__.h = function() { return "1904be93dd639831"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
