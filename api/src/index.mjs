4f70c304a59dea8706959a97c0bad4154f7eb68b
Anurag <anurag79002@gmail.com>
2023-01-30 21:33:08 +0530

    health check route added

--- api/src/index.mjs
+++ api/src/index.mjs
@@ -21,6 +21,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/hello', (_,res) => res.send("HELLO WORLD!"))
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
 app.use('/api/email', emailRouter)
